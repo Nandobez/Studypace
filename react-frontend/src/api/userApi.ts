1617f1a58b6a2a86b655ffaa73c5ac9006df6ad1
luizing <luizeduardo2099@gmail.com>
2025-06-10 15:50:37 -0300

    Front: Auth na requisição final

--- react-frontend/src/api/userApi.ts
+++ react-frontend/src/api/userApi.ts
@@ -24,4 +24,16 @@ export const createUser = (data: any) => axios.post(API_URL, data).then(res => r
 export const updateUser = (id: any, data: any) => axios.put(`${API_URL}/${id}`, data).then(res => res.data);
 export const deleteUser = (id: any) => axios.delete(`${API_URL}/${id}`);
 
-export const sendPrompt = (data: any, id: any) => axios.post(`http://localhost:8080/api/study/${id}/schedule`, data).then(res => res.data);
+export const sendPrompt = (data: any, id: any, token: string) => {
+    console.log(data)
+
+    return axios.post(
+        `http://localhost:8080/api/study/${id}/schedule`,
+        data,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    ).then(res => res.data);
+};
